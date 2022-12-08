42543c71962a175745c2db8f2feb3f7d0558b6ca
jingaisi <jingaisi@outlook.com>
2022-12-08 09:55:12 +0100

    Update ScatterTest5.pro.user

--- secondwindow.cpp
+++ secondwindow.cpp
@@ -1,4 +1,4 @@
-#include "secondwindow.h"
+#include "SecondWindow.h"
 #include "ui_SecondWindow.h"
 #include "qcustomplot.cpp"
 #include <iostream>
@@ -26,10 +26,11 @@ void SecondWindow::makePlot() {
         xx[i] = i; //set x-values
         yy[i] = i; //set y-values
     }
-
     //qDebug() << QString::number(this->x.isEmpty());
 
-    ui->customPlot->addGraph();
+
+
+    /*ui->customPlot->addGraph();
     ui->customPlot->graph(0)->setData(xx,yy);
     ui->customPlot->graph(0)->setLineStyle(QCPGraph::lsNone); //lsNone = no line
     ui->customPlot->graph(0)->setScatterStyle(QCPScatterStyle::ssStar); //ssStar = star points
@@ -38,7 +39,29 @@ void SecondWindow::makePlot() {
     ui->customPlot->yAxis->setLabel("y");
 
     ui->customPlot->xAxis->setRange(0,10); //axis ranges
-    ui->customPlot->yAxis->setRange(0,10);
+    ui->customPlot->yAxis->setRange(0,10);*/
+
+    QCPColorMap *colorMap = new QCPColorMap(ui->customPlot->xAxis, ui->customPlot->yAxis);
+    QCPColorGradient *gradient = new QCPColorGradient();
+
+    //gradient->setLevelCount(20); //sets the amount of discretised colours
+    gradient->setNanHandling(QCPColorGradient::nhNone);
+    gradient->setColorStopAt(0.0, QColor("#ffffff")); //lower gradient colour
+    gradient->setColorStopAt(1.0, QColor("#ff0000")); //upper gradient colour
+
+    colorMap->data()->setSize(100, 100);
+      colorMap->data()->setRange(QCPRange(0, 100), QCPRange(0, 100)); //axis ranges of heat map
+      for (int x=0; x<60; ++x)
+        for (int y=0; y<60; ++y)
+          colorMap->data()->setCell(3, 3, x+y);
+      for (int x=60; x<100; ++x)
+        for (int y=60; y<100; ++y)
+          colorMap->data()->setCell(xx[x], yy[y], x+y);
+      colorMap->setGradient(*gradient);
+      colorMap->rescaleDataRange(true);
+      colorMap->setInterpolate(false);
+      ui->customPlot->rescaleAxes();
+      ui->customPlot->replot();
 }
 
 //C:\Users\leona\OneDrive\Bureau\ScatterTest5\recData.csv
